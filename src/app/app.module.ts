b643f26bedfc80df298a9e90a020d14b8e53ca51
Ketler <ketler13@yandex.ru>
2017-06-13 15:31:38 +0300

    hw3-start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { LetterListComponent } from './letter-list/letter-list.component';
-import { LetterComponent } from './letter-list/letter/letter.component';
-import { RetrospecPipe } from './retrospec.pipe';
-import { SortPipe } from './sort.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LetterListComponent,
-    LetterComponent,
-    RetrospecPipe,
-    SortPipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
