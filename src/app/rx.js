0cb3c7ae58a7740e2b7807999e836df108870d50
Ketler <ketler13@yandex.ru>
2017-06-19 00:53:09 +0300

    blabla

--- src/app/rx.js
+++ src/app/rx.js
@@ -8,49 +8,50 @@ require("rxjs/add/operator/map");
 require("rxjs/add/operator/filter");
 require("rxjs/add/operator/mergeMap");
 
-//----- Создание Observable
-
 const URL = 'https://api.github.com/users?since='
 
-//--creating observable 1
+//-------------creating observable 1
 
-// const source = Observable
-//   .interval(1000)
-//   .mergeMap(_ => (
-//     Observable.fromPromise(axios.get(URL + Math.floor(Math.random()*300)))
-//   ))
-//   .map(res => res.data)
-//   .map(users => users.filter(user => user.id % 3 === 0 && user.login.length > 10))
-//   .take(1);
-//
-// source.subscribe(observer);
+const source = Observable
+  .interval(1000)
+  .mergeMap(_ => (
+    Observable.fromPromise(axios.get(URL + Math.floor(Math.random()*300)))
+  ))
+  .map(res => res.data.filter(user => user.id % 3 === 0 && user.login.length > 10))
+  //.map(users => users.filter(user => user.id % 3 === 0 && user.login.length > 10))
+  .take(1);
 
-//--creating observable 2
-
-const source2 = Observable.create(observer => {
-  const timer = setInterval(() => {
-    const factor = Math.random();
-    console.log(factor)
-    if (factor < 0.8) {
-      observer.next('next value');
-    }
-    if (factor >= 0.8 && factor < 0.9) {
-      throw new Error('Sad but true..');
-    }
-    if (factor >= 0.9) {
-      observer.complete();
-    }
-  }, 2000);
-
-  return function unsubscribe() {
-    clearInterval(timer);
-  }
-});
-
-const subscriber = source2.subscribe(
-  result => console.log(result),
+source.subscribe(
+  users => console.log(users),
   error => console.log(error),
   () => console.log('completed')
 );
+
+//------------creating observable 2
+
+// const source2 = Observable.create(observer => {
+//   const timer = setInterval(() => {
+//     const factor = Math.random();
+//     if (factor < 0.8) {
+//       observer.next('next value');
+//     }
+//     if (factor >= 0.8 && factor < 0.9) {
+//       throw new Error('Sad but true..');
+//     }
+//     if (factor >= 0.9) {
+//       observer.complete();
+//     }
+//   }, 2000);
+//
+//   return function unsubscribe() {
+//     clearInterval(timer);
+//   }
+// });
+//
+// const subscriber = source2.subscribe(
+//   result => console.log(result),
+//   error => console.log(error),
+//   () => console.log('completed')
+// );
 
 //subscriber.unsubscribe();
