7fdac45fd7df3284b4949c5619e252c376d9b5cc
Ketler <ketler13@yandex.ru>
2017-06-24 17:11:59 +0300

    add dumb mail-page route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { UserListComponent } from './user-list/user-list.component';
 import { MailBoxComponent } from './mail-box/mail-box.component';
 import { UserCardComponent } from './user-list/user-card/user-card.component';
 import { UserPageComponent } from './user-list/user-page/user-page.component';
-
-import { UserService } from './user-list/user.service';
 import { MailItemComponent } from './mail-box/mail-item/mail-item.component';
 import { MailPageComponent } from './mail-box/mail-page/mail-page.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+
+import { UserService } from './user-list/user.service';
+import { MailService } from './mail-box/mail.service';
+
 
 const routes: Route[] = [
   { path: 'users', component: UserListComponent },
@@ -41,7 +43,7 @@ const routes: Route[] = [
     HttpModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [UserService],
+  providers: [UserService, MailService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/mail-box/mail-page/mail-page.component.ts
+++ src/app/mail-box/mail-page/mail-page.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
+
+import { MailService } from '../mail.service';
+import { Mail } from '../mail';
 
 @Component({
   selector: 'app-mail-page',
@@ -6,10 +15,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mail-page.component.css']
 })
 export class MailPageComponent implements OnInit {
+  mail: Mail;
+  error = null;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private mailService: MailService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.params
+      .subscribe(params => this.mail = this.mailService.getMail(params.id));
+  }
+
+  goBack(): void {
+    this.router.navigate(['/mail'])
   }
 
 }
