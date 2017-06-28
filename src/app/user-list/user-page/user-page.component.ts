0ac4ce5c221b8d266c231dcbe5ef4becac2d58ec
Ketler <ketler13@yandex.ru>
2017-06-28 22:50:47 +0300

    add user service with get and update methods

--- src/app/user-list/user-page/user-page.component.ts
+++ src/app/user-list/user-page/user-page.component.ts
@@ -2,20 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 
+import { UserService } from '../../user.service';
+import { User } from '../../user'
+
+import 'rxjs/add/operator/switchMap';
+
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.css']
 })
 export class UserPageComponent implements OnInit {
+  user: User;
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.route.params
+      .switchMap(params => this.userService.getUser(params.surname))
+      .subscribe(user => this.user = user);
   }
 
   goBack() {
     this.router.navigate(['/users']);
+  }
+
+  updateInfo() {
+    this.userService.setUserData(
+      {
+        firstName: 'igor'
+      },
+      this.user.surname
+    ).subscribe(updatedUser => this.user = updatedUser)
   }
 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -3,23 +3,44 @@ import { Http } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/take';
 
 import { User } from './user';
 
 @Injectable()
 export class UserService {
   private URL: string;
+  users: object;
 
   constructor(private http: Http) {
-    this.URL = 'https://learn.javascript.ru/courses/groups/api/participants?key=1fxf2pg'
+    this.URL = 'https://learn.javascript.ru/courses/groups/api/participants?key=1fxf2pg';
+    this.users = {};
   }
 
   getUsers(): Observable<User[]> {
     return this.http.get(this.URL)
-      .map(res => res.json() as User[])
+      .map(res => {
+        const users = res.json() as User[];
+        users.forEach(user => this.users[user.surname] = user);
+        return users;
+      })
       .catch(err => Observable.throw(err))
+  }
+
+  getUser(surname: string): Observable<User> {
+    return Observable.of(this.users[surname]);
+  }
+
+  setUserData(data, surname): Observable<User> {
+    this.users[surname] = {
+      ...this.users[surname],
+      ...data
+    }
+    return Observable.of(this.users[surname]);
   }
 
 }
