9a17d3ddceb6ce1fa332e5d6d98a0f1d47bbdf7b
Ketler <ketler13@yandex.ru>
2017-06-30 23:55:25 +0300

    add styles

--- src/app/user-list/user-page/user-page.component.ts
+++ src/app/user-list/user-page/user-page.component.ts
@@ -20,13 +20,20 @@ import 'rxjs/add/operator/do';
 export class UserPageComponent implements OnInit {
   user: User;
   userForm: FormGroup;
+  emailError: boolean;
+  nameError: boolean;
+  surnameError: boolean;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private userService: UserService,
     private formBuilder: FormBuilder
-  ) {}
+  ) {
+    this.emailError = false;
+    this.nameError = false;
+    this.surnameError = false;
+  }
 
   ngOnInit(): void {
     this.route.params
@@ -39,24 +46,37 @@ export class UserPageComponent implements OnInit {
 
   createForm(): void {
     this.userForm = this.formBuilder.group({
-      name: [this.user.name, Validators.required],
-      surname: [this.user.surname, Validators.required],
+      name: [this.user.name, [Validators.required, this.checkLength.bind(this)]],
+      surname: [this.user.surname, [Validators.required, this.checkLength.bind(this)]],
       email: [this.user.email, Validators.required, [this.checkEmailUnique.bind(this)]]
     });
+
+    this.userForm.controls.email.statusChanges
+      .subscribe(status => this.emailError = (status === 'VALID') ? false : true);
+
+    this.userForm.controls.name.statusChanges
+      .subscribe(status => this.nameError = (status === 'VALID') ? false : true);
+
+    this.userForm.controls.surname.statusChanges
+      .subscribe(status => this.surnameError = (status === 'VALID') ? false : true);
   }
 
-  cancel() {
+  cancel(): void {
     this.router.navigate(['/users']);
   }
 
-  submit() {
+  submit(): void {
     this.userService.setUserData(this.userForm.value, this.user.id)
       .switchMap(_ => this.userService.getUser(this.user.id))
       .subscribe(newUser => this.user = newUser);
   }
 
-  checkEmailUnique(formControl) {
+  checkEmailUnique(formControl): Observable<boolean | {}> {
     return this.userService.checkEmailUnique(formControl.value);
+  }
+
+  checkLength(formControl) {
+    return formControl.value.length > 4 ? null : {error: 'small length'}
   }
 
 }
